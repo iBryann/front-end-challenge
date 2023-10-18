4fe5205fae18409b71f9046da0ee935040b80c78
Bryann Henrique <bryann.brandao@sistemapoliedro.com.br>
2023-10-17 22:28:10 -0300

    feat: add lit for dom manipulation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hello world');
+import { html, render } from 'lit';
+const header = document.querySelector<HTMLElement>('.header')!;
+
+const element = html`
+  <h1>Olá!</h1>
+`;
+
+render(element, header);
