91bd0b0c0f105edb69bee4ecc1fd4f70cf6161e5
Bryann Henrique <bryann.brandao@sistemapoliedro.com.br>
2023-10-17 22:58:25 -0300

    feat: add css reseter and sass

--- src/pages/home/script.ts
+++ src/pages/home/script.ts
@@ -1,8 +1,9 @@
 import { html, render } from 'lit';
+
+import './style.scss';
+
 const header = document.querySelector<HTMLElement>('.header')!;
 
-const element = html`
-  <h1>Olá!</h1>
-`;
+const element = html``;
 
 render(element, header);
