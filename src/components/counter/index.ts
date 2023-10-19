121b0d4cc8836182409a15227d557bcec2a84bd0
Bryann Henrique <bryann.brandao@sistemapoliedro.com.br>
2023-10-18 22:13:15 -0300

    refactor: counter

--- src/components/counter/index.ts
+++ src/components/counter/index.ts
@@ -8,29 +8,27 @@ function handleDisableButton(disable: boolean, button: HTMLButtonElement) {
   }
 }
 
-function decrement(button: HTMLButtonElement, input: HTMLInputElement) {
+function operation(
+  option: '-' | '+',
+  button: HTMLButtonElement,
+  input: HTMLInputElement,
+) {
   const min = Number(input?.min || 0);
-  const value = Number(input.value);
-  const newValue = value - 1;
-
-  if (value > min) input.value = String(newValue);
-  handleDisableButton(!newValue, button);
-}
-
-function increment(button: HTMLButtonElement, input: HTMLInputElement) {
   const max = Number(input?.max || 0);
   const value = Number(input.value);
-  const newValue = value + 1;
+  const selectedOption = option === '+';
+  const expression = selectedOption ? value <= max : value > min;
+  const newValue = selectedOption ? value + 1 : value - 1;
 
-  if (value <= max) input.value = String(newValue);
+  if (expression) input.value = String(newValue);
   handleDisableButton(!newValue, button);
 }
 
 document.querySelectorAll('.counter')
   .forEach(element => {
     const [sub, add] = element.querySelectorAll('button');
-    const input = element.querySelector<HTMLInputElement>('input[type="number"')!;
+    const input = element.querySelector<HTMLInputElement>('input[type="number"]')!;
 
-    sub.addEventListener('click', () => decrement(sub, input));
-    add.addEventListener('click', () => increment(sub, input));
+    sub.addEventListener('click', () => operation('-', sub, input));
+    add.addEventListener('click', () => operation('+', sub, input));
   });
